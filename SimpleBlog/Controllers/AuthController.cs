63c902f469645582f1a7ae93589f6fdd83f23c0c
Rostislav Pasechnikov <pasechnikov_ros@hotmail.com>
2015-06-07 16:58:09 +1000

    ViewModels

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,16 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using SimpleBlog.ViewModels;
+using System.Web.Mvc;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
         /// <summary>
-        /// Handle login action
+        /// Handle GET login action
         /// </summary>
         /// <returns></returns>
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin());
+        }
+
+        /// <summary>
+        /// Handles POST login action
+        /// </summary>
+        /// <param name="aForm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Login(AuthLogin aForm)
+        {
+            // IsValid ensures that all Required fields have been populated
+            if(!ModelState.IsValid)
+            {
+                return View(aForm);
+            }
+
+            if(aForm.Username != "error")
+            {
+                ModelState.AddModelError("Username", "Username or password is invalid!");
+                return View(aForm);
+            }
+
+            return Content("The form is valid!");
         }
     }
 }
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
+{
+    public class AuthLogin
+    {
+        // Data sent from the View to the Controller
+        [Required]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
