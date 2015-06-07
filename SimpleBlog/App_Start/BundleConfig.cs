346222b2121eb8606bc2d20b3b9e9fe5cbafa4c9
Rostislav Pasechnikov <pasechnikov_ros@hotmail.com>
2015-06-07 23:24:14 +1000

    Scripts, Styles and Bundles

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Optimization;
+
+namespace SimpleBlog.App_Start
+{
+    public class BundleConfig
+    {
+        /// <summary>
+        /// Registers the style bundles
+        /// </summary>
+        /// <param name="bundles"></param>
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Virtual path - doesn't actually point to anything
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            // Ordering is important!
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.4.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.4.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+        }
+    }
+}
